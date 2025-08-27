0fab1a6e3c82ee8b347d78398439639df161187f
ijiat007 <ijiat007@gmail.com>
2025-08-27 16:14:48 +0900

    Fix

--- src/js/router.js
+++ src/js/router.js
@@ -8,7 +8,26 @@ const routes = {
   "/contact": contact,
 };
 
-export function router(path) {
-  const view = routes[path] || routes["/"];
+// export function router(path) {
+//   const view = routes[path] || routes["/"];
+//   document.querySelector("#page-content").innerHTML = view;
+// }
+
+
+export async function router(path) {
+  const url = routes[path] || routes["/"];
+  const res = await fetch(url);
+  const view = await res.text();
   document.querySelector("#page-content").innerHTML = view;
 }
+
+  // 페이지별 SCSS import (Vite가 CSS로 변환)
+//   switch (path) {
+//     case "/":
+//       import('../pages/home.scss');
+//       break;
+//     case "/about":
+//       import('../pages/about.scss');
+//       break;
+//   }
+// }
