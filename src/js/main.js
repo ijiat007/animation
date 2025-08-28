180070ffe4121a08c831f3624bd51e5e7f78c050
ijiat007 <ijiat007@gmail.com>
2025-08-28 15:42:57 +0900

    Add animate border

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,6 @@
 import "../assets/styles.scss";
+import homeHtml from "../pages/home.html?raw";
 
 const app = document.querySelector("#app");
 
-app.innerHTML = `
-  <header>
-    <h1>Animation Home</h1>
-  </header>
-
-  <h2>Home Page</h2>
-  <p>Welcome to the Home page!</p>
-
-  <footer>
-    <p>© 2025 Animation SPA</p>
-  </footer>
-`;
+app.innerHTML = homeHtml;
