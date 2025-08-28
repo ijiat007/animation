1d8f92fc1e86fcb53dfe442022b39c7bfbc22697
ijiat007 <ijiat007@gmail.com>
2025-08-28 12:53:29 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  base: "/",
+  base: "/animation/",
   resolve: {
     alias: {
       // "@": fileURLToPath(new URL("./src", import.meta.url)),
